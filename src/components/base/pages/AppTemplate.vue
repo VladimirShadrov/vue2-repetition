9ad17b50b2303c217529adf54297f631f3ff0000
Vladimir <shadrov_vladimir@mail.ru>
2023-02-26 12:52:55 +0300

    Контент для Template

--- src/components/base/pages/AppTemplate.vue
+++ src/components/base/pages/AppTemplate.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
     <h1 class="content__theme-title">Свойство template</h1>
+    <br />
+
+    <h3>Компонент: AppTemplate.vue</h3>
+    <br />
+
+    <h4>Задача: Добавить этот HTML в разметку при помощи свойства "template"</h4>
+    <br />
+
+    <h3>Не актуально для компонентов!!!</h3>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'app-template',
+  template: `
+  <div>
+    <h1 class="content__theme-title">Свойство template</h1>
+    <br />
+
+    <h3>Компонент: AppTemplate.vue</h3>
+    <br />
+
+    <h4>Задача: Добавить этот HTML в разметку при помощи свойства "template"</h4>
+    <br />
+  </div>
+  `,
+};
 </script>
 
 <style></style>
