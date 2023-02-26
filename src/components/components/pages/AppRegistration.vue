f579bf3f06526ae643534a347738cccc939457c1
Vladimir <shadrov_vladimir@mail.ru>
2023-02-26 18:46:22 +0300

    Контент для глобальной регистрации компонентов

--- src/components/components/pages/AppRegistration.vue
+++ src/components/components/pages/AppRegistration.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
     <h1 class="content__theme-title">Локальная и глобальная регистрация</h1>
+    <br />
+
+    <h3>Компонент: AppRegistration.vue</h3>
+    <br />
+
+    <h4>Задача: В файле main.js зарегистрировать глобально компонент "testComponents/MyGlobalCounter.vue" и вывести его здесь под чертой</h4>
+    <br />
+
+    <div>
+      <div>
+        Счетчик: <b>{{ counter }}</b>
+      </div>
+      <br /><br />
+      <button @click="counter++" class="button">Увеличить</button>
+    </div>
+    <br /><br />
+    <hr />
+    <br />
   </div>
 </template>
 
 <script>
 export default {
   name: 'app-registration',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
 };
 </script>
 
---
+++ src/components/components/pages/testComponents/MyGlobalCounter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Я глобальный компонент</h2>
+    <br />
+    <div>
+      Счетчик: <b>{{ counter }}</b>
+    </div>
+    <br /><br />
+    <button @click="counter++" class="button">Увеличить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
