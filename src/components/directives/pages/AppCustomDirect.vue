7194f7d3430f376d20a773e8f87a5ed4501a9299
Vladimir <shadrov_vladimir@mail.ru>
2023-02-28 21:13:13 +0300

    Добавлены примеры к собственной директиве

--- src/components/directives/pages/AppCustomDirect.vue
+++ src/components/directives/pages/AppCustomDirect.vue
@@ -10,12 +10,38 @@
       Задача: <br />
       - В папке SRC создать файл directives.js <br />
       - Создать в этом файле объект с названием директивы, которую хотим создать <br />
+      <pre>
+            <code >
+                <textarea readonly="readonly" style="border: none; width: 100%; height: 200px">
+        export const myDirective = {
+            /**
+            *
+            * @param {*} el - элемент, к которому будет применена директива
+            * @param {*} bindings (только для чтения)
+            * @param {*} vnode Нода виртуального дерева (только для чтения)
+            */
+            bind(el, bindings, vnode) {
+                el.style.color = 'blue';
+                bindings;
+                vnode;
+            },
+        };
+                </textarea>
+            </code>
+        </pre>
       - В файле main.js импортировать этот объект <br />
       - До инициализации приложения вызвать у Vue метод "Vue.directive('directive-name', directive-object)" <br />
-      - В объекте создать метод "bind(el, bindings, vnode)". Аргументы метода bind: <br />
-      - el - элемент, к которому будет применена директива <br />
-      - bindings - (только для чтения)<br />
-      - vnode - элемент виртуального дерева (только для чтения)<br />
+      <pre>
+            <code>
+                <textarea readonly="readonly" style="border: none; width: 100%; height: 70px">
+    import { myDirective } from './directive
+
+    Vue.directive('my-directive', myDirective);
+                </textarea>
+            </code>
+        </pre>
+
+      - В объекте создать метод "bind(el, bindings, vnode)" <br />
       - В созданном методе описать действа над элементом (el). Например: el.style.color = 'blue' <br />
       - Применить директиву к элементу шаблона в компоненте. Пример: "h1" v-my-new-directive>Какой-то текст "/h1">
       <br />
--- src/directive.js
+++ src/directive.js
@@ -1,4 +1,10 @@
 export const myDirective = {
+  /**
+   *
+   * @param {*} el - элемент, к которому будет применена директива
+   * @param {*} bindings (только для чтения)
+   * @param {*} vnode Нода виртуального дерева (только для чтения)
+   */
   bind(el, bindings, vnode) {
     el.style.color = 'blue';
     bindings;
