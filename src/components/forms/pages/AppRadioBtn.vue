fa02c5057a8f2f53d7a63eb7e9d09d6700fd4c37
Vladimir <shadrov_vladimir@mail.ru>
2023-03-04 15:26:58 +0300

    Контент для select

--- src/components/forms/pages/AppRadioBtn.vue
+++ src/components/forms/pages/AppRadioBtn.vue
@@ -9,8 +9,8 @@
     <h3>
       Задача: <br />
       - Связать все радио кнопки с моделью <br />
-      - В заголовке вывести выбранные значения <br />
-      - Установить значения по-умолчанию <br />
+      - В заголовке вывести выбранное значение <br />
+      - Установить значение по-умолчанию <br />
     </h3>
     <br /><br />
 
@@ -19,29 +19,24 @@
     <h2>Какую пиццу будем заказывать?</h2>
     <br /><br />
 
-    <label> <input type="radio" value="Сырная" v-model="pizza" /> Сырная </label>
-    <label> <input type="radio" value="Мясная" v-model="pizza" /> Мясная </label>
-    <label> <input type="radio" value="Пеперони" v-model="pizza" /> Пеперони </label>
-    <label> <input type="radio" value="Сырный цыпленок" v-model="pizza" /> Сырный цыпленок </label>
-    <label> <input type="radio" value="Карбонара" v-model="pizza" /> Карбонара </label><br /><br /><br />
+    <label> <input type="radio" /> Сырная </label>
+    <label> <input type="radio" /> Мясная </label>
+    <label> <input type="radio" /> Пеперони </label>
+    <label> <input type="radio" /> Сырный цыпленок </label>
+    <label> <input type="radio" /> Карбонара </label><br /><br /><br />
     <hr />
     <br />
 
     <h4>Вы выбрали:</h4>
     <br />
 
-    <h4>{{ pizza }}</h4>
+    <h4></h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app-radio-buttons',
-  data() {
-    return {
-      pizza: 'Карбонара',
-    };
-  },
 };
 </script>
 
--- src/components/forms/pages/AppSelect.vue
+++ src/components/forms/pages/AppSelect.vue
@@ -2,13 +2,54 @@
   <div>
     <h1 class="content__theme-title">Работа с выпадающими списками</h1>
     <br />
+
+    <h3>
+      Задача: <br />
+      - Создать элементы выпадающего списка <br />
+      - Установить значение по-умолчанию с помощью selected <br />
+      - Связать выпадающий список с моделью <br />
+      - Установить значение по-умолчанию <br />
+      - Вывести выбранный элемент под чертой <br />
+    </h3>
+    <br /><br />
+
+    <!-- РЕАЛИЗАЦИЯ -->
+
+    <select v-model="selected">
+      <option v-for="option in options" :key="option">{{ option }}</option>
+    </select>
+    <br /><br />
+    <hr />
+    <br />
+
+    <h4>Вы выбрали:</h4>
+    <br />
+
+    <h4>{{ selected }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app-select',
+  data() {
+    return {
+      options: ['Сырная', 'Мясная', 'Пеперони', 'Сырный цыпленок', 'Карбонара'],
+      selected: 'Сырный цыпленок',
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+select {
+  width: 200px;
+  padding: 4px 6px;
+  border-radius: 0;
+  outline: none;
+}
+
+option {
+  padding: 6px 3px;
+}
+</style>
