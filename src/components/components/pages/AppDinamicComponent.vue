e477b4fd233218adc5ce571d0c25dad953dcab56
Vladimir <shadrov_vladimir@mail.ru>
2023-03-11 19:27:04 +0300

    Асинхронные компоненты

--- src/components/components/pages/AppDinamicComponent.vue
+++ src/components/components/pages/AppDinamicComponent.vue
@@ -16,46 +16,16 @@
     </h3>
     <br /><br />
 
-    <button @click="active = 1" class="button">Компонент 1</button>
-    <button @click="active = 2" class="button">Компонент 2</button><br /><br /><br />
+    <button class="button">Компонент 1</button>
+    <button class="button">Компонент 2</button><br /><br /><br />
 
     <!-- ЗДЕСЬ ОТРИСОВАТЬ ДИНАМИЧЕСКИЕ КОМПОНЕНТЫ -->
-
-    <!-- <app-dynamic-1 v-if="active === 1"></app-dynamic-1>
-    <app-dynamic-2 v-else-if="active === 2"></app-dynamic-2> -->
-
-    <keep-alive>
-      <component :is="component"></component>
-    </keep-alive>
   </div>
 </template>
 
 <script>
-import AppDynamic1 from './testComponents/AppDynamic1.vue';
-import AppDynamic2 from './testComponents/AppDynamic2.vue';
-
 export default {
   name: 'app-dinamic-component',
-  components: { AppDynamic1, AppDynamic2 },
-  data() {
-    return {
-      active: 1,
-    };
-  },
-  computed: {
-    component() {
-      let component = '';
-      switch (this.active) {
-        case 1:
-          component = 'app-dynamic-1';
-          break;
-        case 2:
-          component = 'app-dynamic-2';
-          break;
-      }
-      return component;
-    },
-  },
 };
 </script>
 
---
+++ src/components/components/pages/testComponents/AppAsyncTest.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <h1 class="content__theme-title">Я асинхронный компонент</h1>
+    <br />
+
+    <h3>Контент асинхронного компонента</h3>
+    <br />
+
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <br />
+
+    <input type="text" placeholder="Введите текст" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-async',
+};
+</script>
+
+<style scoped>
+.wrapper {
+  border: 1px solid black;
+  padding: 16px;
+}
+</style>
