2b47aaae68ef3e92d61703f245e61352cf1bb8b9
Vladimir <shadrov_vladimir@mail.ru>
2023-03-13 21:15:38 +0300

    Очитска решения

--- src/components/filters/pages/AppFilterLists.vue
+++ src/components/filters/pages/AppFilterLists.vue
@@ -14,13 +14,12 @@
     </h3>
     <br /><br />
 
-    <input v-model="inputValue" type="text" class="input" /><br /><br />
+    <input type="text" class="input" /><br /><br />
     <hr />
     <br />
 
-    <p class="list-item" v-for="name in listArr" :key="name">
-      <!-- <b>Сюда вывести список</b> -->
-      <b> {{ name }} </b>
+    <p class="list-item">
+      <b>Сюда вывести список</b>
     </p>
   </div>
 </template>
@@ -34,11 +33,6 @@ export default {
       placeholder: 'Введите имя...',
       inputValue: '',
     };
-  },
-  computed: {
-    listArr() {
-      return this.names.filter((name) => name.toLowerCase().includes(this.inputValue.toLowerCase()));
-    },
   },
 };
 </script>
